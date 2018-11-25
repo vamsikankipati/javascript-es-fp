d496dec2a5777306c9453991aec2953996872b00
Vamsi Kankipati <vamsibh1@gmail.com>
2018-11-25 12:11:07 +0530

    Arrow function and template literal

--- src/basics/es6.js
+++ src/basics/es6.js
@@ -1,3 +1,8 @@
 let a = () => {
   console.log("let keyword and Arrow Functions");
 }
+
+let message = name => `Hello ${name}`;
+
+//This is run using atom runner package for easier testing.
+console.log(message('Vamsi'));
