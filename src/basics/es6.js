3bb9deb8185322b9b7fba02c5f75a3fdca76d2b7
Vamsi Kankipati <vamsibh1@gmail.com>
2018-12-08 16:43:49 +0530

    removal of unwanted files

--- src/basics/es6.js
+++
@@ -1,8 +0,0 @@
-let a = () => {
-  console.log("let keyword and Arrow Functions");
-}
-
-let message = name => `Hello ${name}`;
-
-//This is run using atom runner package for easier testing.
-console.log(message('Vamsi'));
