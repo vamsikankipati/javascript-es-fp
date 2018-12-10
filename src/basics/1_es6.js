834cbc55671a7071b16fe34cf6a27cd6e84951c0
Vamsi Kankipati <vamsibh1@gmail.com>
2018-12-10 23:59:09 +0530

    ES6 features

--- src/basics/1_es6.js
+++ src/basics/1_es6.js
@@ -1,8 +1,10 @@
+// let keyword along with Arrow functions
 let a = () => {
   console.log("let keyword and Arrow Functions");
 }
 
+// let keyword
 let message = name => `Hello ${name}`;
 
-//This is run using atom runner package for easier testing. (Alt + R)
+// This is run using atom runner package for easier testing. (Alt + R)
 console.log(message('Vamsi'));
