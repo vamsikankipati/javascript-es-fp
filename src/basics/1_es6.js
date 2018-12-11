4a959bcacd4db311bac626ba97628c055a4591ce
Vamsi Kankipati <vamsibh1@gmail.com>
2018-12-11 09:22:19 +0530

    let var functionalitites

--- src/basics/1_es6.js
+++ src/basics/1_es6.js
@@ -8,3 +8,14 @@ let message = name => `Hello ${name}`;
 
 // This is run using atom runner package for easier testing. (Alt + R)
 console.log(message('Vamsi'));
+
+// let is blockscoped
+// var is function scoped
+
+  /*At first all the declarations are processed by the
+    Javascript engine before executing the code
+  */
+
+/* Declaration is pocessed during compilation phase and initialization is processed
+   during execution */
+   var b = 5; // declaration and initialization of variable b
