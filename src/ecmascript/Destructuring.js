e667709637d4284e4395d4046a4dbcf15ede769a
Vamsi Kankipati <vamsibh1@gmail.com>
2019-07-17 23:28:11 +0530

    es6 destrcuturing assignment

---
+++ src/ecmascript/Destructuring.js
@@ -0,0 +1,14 @@
+//Destructured assignment
+let user = {
+  userId: 31,
+  userName: vamsi31,
+  userContact: 12345
+};
+
+function getUserId({userId}) {
+  return userId;
+}
+
+//The curly braces unpacks the object attributes
+getUserId(user); // 31
+
