015f2aeaef250f21e49b383b32aa42a21f8196a7
Vamsi Kankipati <vamsibh1@gmail.com>
2019-07-27 23:40:18 +0530

    Promise object

--- src/basics/PromiseObject.js
+++ src/basics/PromiseObject.js
@@ -1,6 +1,6 @@
 
 // Solution for Asynchronous programming - alternative to callbacks and events
-const p = new Promise(function(resolve, reject) {
+const promise = new Promise(function(resolve, reject) {
 	if(success) {
 		resove(value);
 	} else {
